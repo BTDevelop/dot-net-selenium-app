b7aac49a52eebdc9268d5b6c0288c67130eda14b
Burak Tahtacıoğlu <clubwomanizer588@gmail.com>
2018-12-27 16:13:11 +0300

    initial project

---
+++ SeleniumConsole.App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumDriver.Service;
+
+namespace SeleniumConsole.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeleniumEngine seleniumEngine = new SeleniumEngine("https://www.sahibinden.com/satilik");
+
+            seleniumEngine.Navigate();
+            string result = seleniumEngine.Search();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SeleniumDriver.Service/SeleniumEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDriver.Service
+{
+    public class SeleniumEngine
+    {
+        IWebDriver driver = new ChromeDriver(@"C:\\Users\\Burak Tahtacıoğlu\\source\\repos\\dot-net-selenium-app\\SeleniumDriver.Service\\bin\\Debug"); //change
+        public readonly string navigateURL;
+
+        public SeleniumEngine(string navigateURL)
+        {
+            this.navigateURL = navigateURL;
+        }
+
+        public void Navigate()
+        {
+            driver.Navigate().GoToUrl(this.navigateURL);
+        }
+
+        public string Search()
+        {
+            driver.FindElement(By.XPath("//*[@class='address-select-wrapper real-estate']/li[1]/a")).Click();
+            string findValue = driver.FindElement(By.XPath("//*[@data-id='6']")).Text;
+
+            driver.Close();
+            return findValue;
+        }
+    }
+}
